badcd426cd874ac91462461052d3bcf18041e363
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2016-01-24 11:44:44 +0530

    Disable invokedynamic, rename scripts

---
+++ tools/bin/jruby.bat
@@ -0,0 +1,7 @@
+set JRUBY_BASE=%~dp0\..\jruby
+set GEM_HOME=
+set GEM_PATH=
+set PATH=%JRUBY_BASE%\bin;%PATH%
+
+set JRUBY_OPTS='-J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1 -J-Djruby.compile.invokedynamic=false -J-Djruby.compile.mode=OFF'
+%JRUBY_BASE%\bin\jruby.bat %*
