464933524da281fb163096e130cb93842edc4a29
Marques Lee <marques.lee@thoughtworks.com>
2019-09-18 16:57:44 -0700

    I obviously have no idea of what I'm doing Revert "Output what the response body is when JSONArray fails to parse the payload"
    This reverts commit 5113f34be3b7034a91c0fd40eb796d432edec95b.

--- src/test/java/com/thoughtworks/cruise/api/UsingPipelineApi.java
+++ src/test/java/com/thoughtworks/cruise/api/UsingPipelineApi.java
@@ -116,15 +116,7 @@ public class UsingPipelineApi {
 	private String getMaterialFingerprint(String material) throws JSONException {
 
 		CruiseResponse response = talkToCruise.get(Urls.urlFor("/api/config/materials"));
-		JSONArray jsonArray = null;
-
-		try {
-			jsonArray = new JSONArray(response.getBody());
-		} catch (JSONException e) {
-			System.err.println(response.getBody());
-			throw e;
-		}
-
+		JSONArray jsonArray = new JSONArray(response.getBody());
 		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject obj = jsonArray.getJSONObject(i);
 			if (obj.get("description").toString().contains(material)) {
